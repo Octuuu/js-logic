8b9715942711e8279218d0d5cc434cff137693ac
Octuuu <nosi98758@gmail.com>
2025-03-10 10:50:02 -0400

    practica

--- ejer2.js
+++ ejer2.js
@@ -14,30 +14,6 @@ function repeticion(arr){
 
 console.log(repeticion([2,3,4,5,3,3]));
 
-/*
-function repeticion(arr){
-    let frecuencia = {};
-    let maxNum = arr[0]; maxF = 0;
-
-    for (let num of arr) {
-        frecuencia[num] = (frecuencia[num] || 0) + 1;
-        if (frecuencia[num] > maxF) {
-            maxNum = num;
-            maxF = frecuencia[num];
-        }
-    }
-    return {num: maxNum, frecuencia: maxF};
-}
-
-<<<<<<< HEAD
-console.log(repeticion([1, 3, 2, 3, 3, 1, 5, 1, 3])); 
-console.log(repeticion([7, 8, 9, 7, 8, 8, 7, 7]));
-
-=======
-console.log(repeticion([1, 3, 2, 3, 3, 1, 5, 1, 3])); // { elemento: 3, repeticiones: 4 }
-console.log(repeticion([7, 8, 9, 7, 8, 8, 7, 7])); // { elemento: 7, repeticiones: 4 }
-*/
->>>>>>> 8d6cd48 (practica)
 
 /*
 2️⃣ Encontrar el Elemento que Más Se Repite en un Array
