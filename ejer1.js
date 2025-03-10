d49a3cfd7c3382b5823b898f4c4e373e50e7871d
Octuuu <nosi98758@gmail.com>
2025-03-10 10:55:06 -0400

    practica2

--- ejer1.js
+++ ejer1.js
@@ -15,26 +15,7 @@ function caractersMax(s) {
 
 console.log(caractersMax('abcabcbb'))
 
-
-
 /*
-function caractersMax(s){
-    let set = new Set();
-    let maxLength = 0, izquierda = 0;
-
-    for (let derecha = 0 ; derecha <= s.length ; derecha++) {
-        while (set.has(s[derecha])){
-            set.delete(s[izquierda]);
-            izquierda++;
-        }
-        set.add(s[derecha]);
-        maxLength = Math.max(maxLength, derecha - izquierda + 1);
-    }
-    return maxLength;
-}
-
-
-console.log(caractersMax("abcabcbb")); // 3
 */
 // Encontrar la Subcadena Más Larga Sin Repeticiones
 //📌 Dado un string, encuentra la longitud de la subcadena más larga sin caracteres repetidos.
